46a933195ecfb92cca16c826352c8f69f373d94a
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2023-01-09 16:38:31 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+
+        for (int i = 1; i <= 20; i++) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+
+            stack1.push(i);
+
+            while (!stack2.isEmpty()) {
+                stack1.push(stack2.pop());
+            }
+        }
+
+        while (!stack1.isEmpty()) {
+            System.out.print(stack1.pop() + " ");;
+        }
     }
 }
